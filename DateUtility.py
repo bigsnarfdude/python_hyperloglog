1ba86f2bade9745e80af12777c225e49bcabd274
BigsnarfDude <bigsnarfdude@users.noreply.github.com>
2025-01-19 23:25:06 -0700

    Update DateUtility.py

--- DateUtility.py
+++ DateUtility.py
@@ -5,8 +5,21 @@ class DateUtility:
    @staticmethod
    def bucket(date: datetime) -> str:
        """
-       Bucket a datetime by reducing precision to the minute.
-       Downsamples datetime to minute precision in ISO 8601 format.
+         
+         Object uses downsampling method to create metadata from each 
+         EventType log record. Parsing the ISO 8601 
+         datetime stamp to the minute means downsampling aka reducing 
+         precision.
+         
+         Bucketing
+         A family of aggregations that build buckets, where each bucket 
+         is associated with a key and an EventType criterion. When the 
+         aggregation is executed, all the buckets criteria are evaluated 
+         on every EventType in the context and when a criterion matches, 
+         the EventType is considered to "fall in" the relevant bucket. 
+         By the end of the aggregation process, we’ll end up with a 
+         list of buckets - each one with a set of EventTypes that 
+         "belong" to it.
        
        Args:
            date: datetime object to bucket
